4fbcd79946688e73b86e5ddd7dd344b84cd11950
zmeu <tsraduu@gmail.com>
2023-11-24 16:02:21 +0000

    Fonts added to tailwing.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@
 module.exports = {
   content: ["./**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+        montserat: ["Montserrat", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
   },
   purge: ["./**/*.{html,js}", "./src/**/*.{html,js}"], // Add the purge option
   plugins: [],
