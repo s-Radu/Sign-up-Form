d01a06f9b7a1f9e9a881f0993a95e12297e8b2cc
zmeu <tsraduu@gmail.com>
2023-11-25 08:47:45 +0000

    Added special thank to the Readme file

--- README.md
+++ README.md
@@ -13,6 +13,15 @@ The form is just a simple sign-up form and I've added a sign in page that will t
 
 This project is not licenced and it's part of the Odin Project learning trail
 
+## Image credits
+
+- Background photos have been taken from Unsplash.
+  Special thanks to Tyler Quick
+  Special thanks to Kajetan Suilma
+
+- The fav icon was taken from icon archive.
+  Special thanks to Erix Subyarko
+
 ## Authors
 
 Radu ( lil void)
--- src/app.js
+++ src/app.js
@@ -8,14 +8,10 @@ function changePage() {
   const firstPage = document.getElementById("firstPage");
   const secondPage = document.getElementById("secondPage");
   if (firstPage.classList.contains("slideIn")) {
-    firstPage.classList.remove("slideIn");
-    firstPage.classList.add("slideOut");
-    secondPage.classList.add("slideIn");
-    secondPage.classList.remove("slideOut");
+    firstPage.className = "slideOut";
+    secondPage.className = "slideIn";
   } else {
-    firstPage.classList.add("slideIn");
-    firstPage.classList.remove("slideOut");
-    secondPage.classList.remove("slideIn");
-    secondPage.classList.add("slideOut");
+    firstPage.className = "slideIn";
+    secondPage.className = "slideOut";
   }
 }
